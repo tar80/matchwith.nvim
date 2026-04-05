1baa7d7c0042ca54c2692fc678e1d7c6388e89eb
tar80 <tar80master@gmail.com>
2026-04-05 11:29:56 +0900

    fix: fix: correct nvim version check logic (4a10cae)

--- lua/matchwith/keymap.lua
+++ lua/matchwith/keymap.lua
@@ -44,7 +44,7 @@ function M.setup(UNIQUE_NAME, Cache)
   end, { desc = ('%s: select matchpair range'):format(UNIQUE_NAME) })
   vim.keymap.set({ 'o', 'x' }, '<Plug>(matchwith-operator-parent-i)', function()
     operator_matchpair('i', true)
-    if vim.fn.has('nvim-0.13') == 0 then
+    if vim.fn.has('nvim-0.13') == 1 then
       vim.deprecate(
         '<Plug>(matchwith-operator-parent-i)',
         'the builtin Treesitter "in"',
@@ -55,7 +55,7 @@ function M.setup(UNIQUE_NAME, Cache)
   end, { desc = ('%s: select inner matchpair'):format(UNIQUE_NAME) })
   vim.keymap.set({ 'o', 'x' }, '<Plug>(matchwith-operator-parent-a)', function()
     operator_matchpair('a', true)
-    if vim.fn.has('nvim-0.13') == 0 then
+    if vim.fn.has('nvim-0.13') == 1 then
       vim.deprecate(
         '<Plug>(matchwith-operator-parent-a)',
         'the builtin Treesitter "an"',
